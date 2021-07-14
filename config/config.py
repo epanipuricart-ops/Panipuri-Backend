eaf192302b22144104d031484e47cbc1aad645e8
TheSpeedX <ggspeedx29@gmail.com>
2021-07-14 13:12:24 +0530

    updated token

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ IOTFlask = {
 }
 
 ZohoConfig = {
-    "refresh_token": "1000.7f91c651dcd68a7d5e8efbb8c75cc8c7.c00923092ff3a466eb3592656afd6284",
+    "refresh_token": "1000.8c2e69d900ca20ca1327ae6af4372cf2.377472d9e4631a416b0f7e6a8da02cd3",
     "client_id": "1000.TM9579QXAQRONAPUYM8I9V3JR1AIZY",
     "client_secret": "3e4811b9ced7df56e36e1e5090c49b012c03092fb9",
     "organization_id": "60003188756"
